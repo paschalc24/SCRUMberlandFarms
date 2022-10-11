e9a2b48e40bdcdd72537a0b821bfc0707597ae2c
paschalc24 <pcorrigan@umass.edu>
2022-10-11 17:55:08 -0400

    Created grid structure, filled with temp calendars

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,25 @@ import BasicExample from "./UIComponents/navbar.js";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
       <BasicExample />
-      <Calendar className = "cal"/>
-      <button className="mainButtons">New Goal</button>
-      <button className="mainButtons">View Past Goals</button>
+      <Container fluid className="grid">
+        <Row>
+          <Col sm={8}>
+          <Calendar className = "cal2"/>
+          </Col>
+          <Col sm={4}>
+          <Calendar className = "cal"/>
+          <Calendar className = "cal"/>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
