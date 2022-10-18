3fc94d88bff136c7922abcb52bd1e9cc1852c58d
jasonjermany <67882440+jasonjermany@users.noreply.github.com>
2022-10-18 17:32:34 -0400

    calendar filtering

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+import React, { useState } from 'react';
+
 import Header from "./UIComponents/Header.js";
-import Calendar from 'react-calendar';
+// import Calendar from 'react-calendar';
 import GoalsList from "./UIComponents/GoalsList.js";
 import GoalsCalendarList from "./UIComponents/GoalsCalendarList.js";
+import Cal from "./UIComponents/Calendar.js";
 
 import 'react-calendar/dist/Calendar.css';
-import './CSSComponents/Calendar.css';
+// import './CSSComponents/Calendar.css';
 import './CSSComponents/Grid.css';
 
 import './App.css';
@@ -13,6 +16,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function App() {
+  const [value, setValue] = useState(new Date());
+
   return (
     <div className="App">
       <Container fluid className="vh-100 d-flex flex-column">
@@ -20,8 +25,8 @@ function App() {
         <Row className="mainRow">
           <Col sm={9}><GoalsList/></Col>
           <Col sm={3}>
-            <Calendar className = "cal"/>
-            <GoalsCalendarList/>
+            <Cal value={value} setValue={setValue}/>
+            <GoalsCalendarList value={value}/>
           </Col>
         </Row>
       </Container>
--- client/src/UIComponents/GoalsCalendarList.js
+++ client/src/UIComponents/GoalsCalendarList.js
@@ -1,11 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import '../CSSComponents/goalsCalendarList.css'
+import data from '../tempStorage.json';
 
 
-function GoalsCalendarList() {
+function GoalsCalendarList({value}) {
+    let filterGoals = data.users[0].goals.filter(goal => Date.parse(goal.goalEndDate) === Date.parse(value));
+    let goalNames = filterGoals.map(goal => goal.goalName);
+
     let state = {
-        goals: ['Test Goal 1']
+        goals: goalNames
     };
+
     return (
         <React.Fragment>
         <ul className="goals-calendar-group">
