4d87d0c72030a6604a8daa619f4ac3566e876d4a
paschalc24 <pcorrigan@umass.edu>
2022-10-19 20:48:20 -0400

    Added description to GoalsCalList state.

--- client/src/UIComponents/GoalsCalendarList.js
+++ client/src/UIComponents/GoalsCalendarList.js
@@ -6,9 +6,11 @@ import data from '../tempStorage.json';
 function GoalsCalendarList({value}) {
     let filterGoals = data.users[0].goals.filter(goal => Date.parse(goal.goalEndDate) === Date.parse(value));
     let goalNames = filterGoals.map(goal => goal.goalName);
+    let descriptions = filterGoals.map(goal => goal.goalDesc);
 
     let state = {
-        goals: goalNames
+        goals: goalNames,
+        descriptions: descriptions,
     };
 
     return (
@@ -16,9 +18,16 @@ function GoalsCalendarList({value}) {
         <ul className="goals-calendar-group">
             {state.goals.map(goal => (
                 <li 
-                key = {goal}
+                key = {goal[0]}
                 className="goal-item">
                 {goal} 
+                </li>
+            ))}
+            {state.descriptions.map(description => (
+                <li 
+                key = {description}
+                className="goal-item">
+                {description} 
                 </li>
             ))}
         </ul>
