853ee73de746dc024a1f799156ce3a5c418e2429
paschalc24 <pcorrigan@umass.edu>
2022-10-24 15:13:59 -0400

    Added Card Component for Description.

---
+++ client/src/UIComponents/card.js
@@ -0,0 +1,32 @@
+import Card from 'react-bootstrap/Card';
+
+function BgColorExample() {
+  return (
+    <>
+      {[
+        'Light'
+      ].map((variant) => (
+        <Card
+          bg={'Light'.toLowerCase()}
+          key={'Light'}
+          text={'Light'.toLowerCase() === 'light' ? 'dark' : 'white'}
+          style={{ width: '100%', height: '100%' }}
+          className="mb-2"
+        >
+          <Card.Header>Goal Title</Card.Header>
+          <Card.Body>
+            <Card.Title> Status </Card.Title>
+            <Card.Text>
+              Description <br></br>
+              Started 10/22/22. <br></br>
+              Finished 10/23/22. <br></br>
+              Assigned by John Doe.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </>
+  );
+}
+
+export default BgColorExample;
