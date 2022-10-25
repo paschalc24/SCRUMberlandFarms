8616bbe520a74eaf58a0ed84455f6c6abf9af2c6
jasonjermany <67882440+jasonjermany@users.noreply.github.com>
2022-10-25 10:28:36 -0400

    delete confirmation

--- client/src/UIComponents/DeleteGoal.js
+++ client/src/UIComponents/DeleteGoal.js
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { CgTrash } from 'react-icons/cg';
 import '../CSSComponents/deleteGoal.css';
 
 export default function DeleteGoal(props) {
+
+    const [show, setShow] = useState(false);
+
+    const handleCloseYes = () => {
+        deleteRow(props.title);
+        setShow(false);
+    };
+
+    const handleCloseNo = () => setShow(false);
+
+    const handleShow = () => setShow(true);
+
     const deleteRow = (title) => {
         const newList = props.rows.filter((item) => item.title !== title);
         props.setGoals(newList);
@@ -10,8 +24,25 @@ export default function DeleteGoal(props) {
     };
 
     return (
-        <button className="delete" onClick={() => deleteRow(props.title)}>
-            <CgTrash size={18}/>
-        </button>
+        <>
+            <button className="delete" onClick={handleShow}>
+                <CgTrash size={18}/>
+            </button>
+
+            <Modal show={show} onHide={handleCloseNo}>
+            <Modal.Header closeButton>
+                <Modal.Title>Delete Goal</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Are you sure you want to delete this goal?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseNo}>
+                        No
+                    </Button>
+                    <Button className="yesButton" variant="primary" onClick={handleCloseYes}>
+                        Yes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     )
 }
