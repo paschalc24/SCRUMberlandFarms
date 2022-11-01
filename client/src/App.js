3d369751b4dd2373556fc9aaaae2af995a69c939
paschalc24 <pcorrigan@umass.edu>
2022-11-01 16:30:08 -0400

    Added Manager and Employee Views.

--- client/src/App.js
+++ client/src/App.js
@@ -20,25 +20,48 @@ import CollapsibleTable from "./UIComponents/GoalsList2.js";
 
 function App() {
   const [value, setValue] = useState(new Date());
-
-  return (
-    <div className="App">
-      <Container fluid> 
-        <Row><Header/></Row>
-        <Row className="mainRow">
-          <Col sm={9}>
-            <CollapsibleTable/>
-          </Col>
-          <Col sm={3}>
-            <Row className="calRow"><Cal value={value} setValue={setValue}/></Row>
-            {/* <Row className="descRow"><GoalDescription/></Row> */}
-            {/* <Row className="descRow"><GoalsCalendarList value={value}/></Row> */}
-            <GoalsCalendarList value={value}/>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+  const managerView = true;
+  const viewTitle = managerView ? "Manager View": "Employee View";
+  if (managerView) {
+    return (
+      <div className="App">
+        <Container fluid> 
+          <Row><Header view={managerView} vTitle={viewTitle}/></Row>
+          <Row className="mainRow">
+            <Col sm={9}>
+              <CollapsibleTable/>
+            </Col>
+            <Col sm={3}>
+              <Row className="calRow"><Cal value={value} setValue={setValue}/></Row>
+              {/* <Row className="descRow"><GoalDescription/></Row> */}
+              {/* <Row className="descRow"><GoalsCalendarList value={value}/></Row> */}
+              <GoalsCalendarList value={value}/>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="App">
+        <Container fluid> 
+          <Row><Header view={managerView} vTitle={viewTitle}/></Row>
+          <Row className="mainRow">
+            <Col sm={9}>
+              <CollapsibleTable/>
+            </Col>
+            <Col sm={3}>
+              <Row className="calRow"><Cal value={value} setValue={setValue}/></Row>
+              {/* <Row className="descRow"><GoalDescription/></Row> */}
+              {/* <Row className="descRow"><GoalsCalendarList value={value}/></Row> */}
+              <GoalsCalendarList value={value}/>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
