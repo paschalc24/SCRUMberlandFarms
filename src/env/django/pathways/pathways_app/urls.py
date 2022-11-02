09b8561d351e1da61fae144405680760133b4154
Eric4K <ejhuang@umass.edu>
2022-11-02 16:31:21 -0400

    add update and delete to employee

--- src/env/django/pathways/pathways_app/urls.py
+++ src/env/django/pathways/pathways_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     # configured the url to call the corresponding endpoints methods
     path('employee/get/', views.getEmployee),
     path('employee/post/', views.postEmployee),
+    path('employee/update/', views.updateEmployee),
+    path('employee/delete/', views.deleteEmployee),
     path('goals/delete/', views.deleteGoal),
     path('goals/post/', views.postGoal),
     path('goals/get/', views.getGoal)
