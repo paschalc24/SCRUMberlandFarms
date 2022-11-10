39d7ff9bec1a56a02ca7d87822b1e1f854ecb7b6
Eric4K <ejhuang@umass.edu>
2022-11-09 23:04:16 -0500

    receive params via url

--- src/env/django/pathways/pathways_app/serializers.py
+++ src/env/django/pathways/pathways_app/serializers.py
@@ -6,13 +6,13 @@ from .models import employee, goal, comment
 class employeeSerializer(serializers.ModelSerializer):
     firstName = serializers.CharField()
     lastName = serializers.CharField()
-    email = serializers.CharField(default="@gmail.com")
-    companyName = serializers.CharField(default="inc")
-    managerId = serializers.IntegerField(default="321")
-    positionTitle = serializers.CharField(default="Employee")
-    startDate = serializers.DateField(default="2023-02-22")
-    isManager = serializers.BooleanField(default=False)
-    password = serializers.CharField(default="password")
+    email = serializers.CharField()
+    companyName = serializers.CharField()
+    managerId = serializers.CharField()
+    positionTitle = serializers.CharField()
+    startDate = serializers.DateField()
+    isManager = serializers.BooleanField()
+    password = serializers.CharField()
     class Meta:
         model = employee
         fields = '__all__'
