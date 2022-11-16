169563129777c30e7c0a0d5f5e2ef8079c9dc179
jasonjermany <67882440+jasonjermany@users.noreply.github.com>
2022-11-16 17:41:55 -0500

    urls main

--- src/env/django/pathways/pathways_app/urls.py
+++ src/env/django/pathways/pathways_app/urls.py
@@ -10,12 +10,10 @@ urlpatterns = [
     path('goals/delete/', views.deleteGoal),
     path('goals/post/', views.postGoal),
     path('goals/get/', views.getGoal),
-<<<<<<< HEAD
-    path('goals/update/', views.updateGoal)
-=======
+    path('goals/update/', views.updateGoal),
     path('comments/post/', views.postComment),
     path('comments/get/', views.getComments),
     path('comments/delete/', views.deleteComments),
-    path('comments/update/', views.updateComment)
->>>>>>> main
+    path('comments/update/', views.updateComment),
+    path('manager/get/', views.getManager),
 ]
